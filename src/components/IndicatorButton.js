21258d52598422ea9b980717a502304b6a05cd72
Aiden Laptop <aidenmawji@gmail.com>
2022-08-22 19:15:37 -0400

    added btns for switching graphs on EconIndicators page

---
+++ src/components/IndicatorButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndicatorButton = ({}) => {
+  return (
+    <button>{title}</button>
+  )
+}
+
+export default IndicatorButton
--- src/pages/EconomicIndicators.js
+++ src/pages/EconomicIndicators.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const EconomicIndicators = () => {
   return (
-    <div style={{width: '84vw'}} >EconomicIndicators</div>
+    <div style={{width: '84vw'}} >
+      
+      <button>Real GDP</button>
+      <button>10-Year Treasury Yield</button>
+      <button>Federal Funds Rate</button>
+      <button>Consumer Price Index</button>
+      <button>Inflation Rate</button>
+      <button>Unemployment rate</button>
+
+    </div>
   )
 }
 

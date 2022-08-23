6ebe13b5ef90f9a17c5d36d6ba72c3f5eec915e8
Aiden Laptop <aidenmawji@gmail.com>
2022-08-22 23:21:43 -0400

    modified css to encompass flexbox rules for tooling down the road

--- src/pages/EconomicIndicators.js
+++ src/pages/EconomicIndicators.js
@@ -4,12 +4,14 @@ const EconomicIndicators = () => {
   return (
     <div style={{width: '84vw'}} >
       
-      <button>Real GDP</button>
-      <button>10-Year Treasury Yield</button>
-      <button>Federal Funds Rate</button>
-      <button>Consumer Price Index</button>
-      <button>Inflation Rate</button>
-      <button>Unemployment rate</button>
+      <div style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+        <button>Real GDP</button>
+        <button>10-Year Treasury Yield</button>
+        <button>Federal Funds Rate</button>
+        <button>Consumer Price Index</button>
+        <button>Inflation Rate</button>
+        <button>Unemployment rate</button>
+      </div>
 
     </div>
   )

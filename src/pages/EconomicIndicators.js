3b7bd2bbae7facef6fb3f45b9757f7c993c0ec7d
Aiden Laptop <aidenmawji@gmail.com>
2022-08-23 15:21:08 -0400

    started to implement tooling for fetching, and getting access to the data I need

--- src/pages/EconomicIndicators.js
+++ src/pages/EconomicIndicators.js
@@ -4,7 +4,26 @@ import IndicatorButton from '../components/IndicatorButton'
 
 const EconomicIndicators = () => {
 
-  
+  const getEcon = async ( apiUrl ) => {
+    const data = await fetch(apiUrl); //CS2FWWWNNNJOW6BT
+    const econ = await data.json();
+    // console.log(econ.data);
+    // console.log(Object.values(econ.data[0]));
+    // console.log(econ.name);
+    // console.log(econ.unit);
+    // console.log(econ.interval);
+
+    for(let i = 0; i < econ.data.length; i++) {
+      console.log(Object.values(econ.data[i]))
+    }
+
+    // let tempObj = { date: dates[i], value: prices[i] };
+  } 
+
+  useEffect(() => {
+    getEcon(`https://www.alphavantage.co/query?function=REAL_GDP&interval=annual&apikey=demo`);
+    console.log('call use effect once')
+  }, [])
   
 
 

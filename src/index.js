752500a64c8917e41c13f5494aae1d80c6f75b36
Aiden Laptop <aidenmawji@gmail.com>
2022-08-25 12:10:50 -0400

    switched back to browser router but in the index

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter} from 'react-router-dom'
+import { BrowserRouter, HashRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   // </React.StrictMode>
 );
 
